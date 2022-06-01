8a6ae4f596ac7e5a547814ace632a6aebf7f7bb9
MayaSelena773 <maya.bono721@gmail.com>
2022-06-01 17:37:24 +0200

    firstif

--- js/script.js
+++ js/script.js
@@ -7,19 +7,29 @@
 //L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 
-//Chiedere all'utente 
-//Moltiplicare il prezzo base del bilgietto (0.21 € al km) per il numero dei kilometri
-//Se il passeggero è minorenne applicare lo sconto del 20%
-//Se il passeggero è over 65 applicare lo sconto del 40%
-//Output prezzo finale da stampare in pagina html approssimato a due numeri decimali dopo la virgola
+
 
 //Chiedo all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
-const userKilometres = prompt("Scrivi qui i chilometri da percorrere");
-const userAge = prompt("Scrivi qui l'età del passeggero");
+const userKilometres = parseInt( prompt("Scrivi qui i chilometri da percorrere"));
+console.log(userKilometres);
+const userAge = parseInt( prompt("Scrivi qui l'età del passeggero"));
+console.log(userAge);
 
 //Moltiplicare il prezzo base del bilgietto (0.21 € al km) per il numero dei chilometri
-let baseTicket = userKilometres * 0.21 €;
+let baseTicket = userKilometres * 0.21;
 console.log(baseTicket);
+
+//Se il passeggero è minorenne applicare lo sconto del 20%
+
+let discountTicketMin;
+
+if (userAge < 18)
+discountTicketMin = baseTicket * 2 / 100;
+console.log(discountTicketMin);
+
+
+//Se il passeggero è over 65 applicare lo sconto del 40%
+//Output prezzo finale da stampare in pagina html approssimato a due numeri decimali dopo la virgola
 
 
 
